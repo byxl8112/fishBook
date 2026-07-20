bc870305a78a9fa08a3b3f5f3d8cbb6f52bde46f
byxl8112 <byxl8112@gmail.com>
2026-07-20 22:44:43 +0800

    fix：修改注释理解

--- ch04/train_neuralnet.py
+++ ch04/train_neuralnet.py
@@ -20,9 +20,9 @@ train_size = x_train.shape[0]  # 训练数据总数有多少
 batch_size = 100  # 每次给网络 100 张图片来训练，即 Mini-batch
 learning_rate = 0.1  # 学习率
 
-train_loss_list = []
-train_acc_list = []
-test_acc_list = []
+train_loss_list = []  # 保存 loss，以后画 loss 曲线
+train_acc_list = []  # 保存准确率
+test_acc_list = []  # 测试准确率
 
 # 算出多少步为一个 epoch（意味着模型每走 600 步，相当于把 60000张图片轮流看了一遍），看完整套教材一次，就叫一个 epoch
 iter_per_epoch = max(train_size / batch_size, 1)
@@ -44,15 +44,15 @@ for i in range(iters_num):
     train_loss_list.append(loss)
 
     if i % iter_per_epoch == 0:  # 每经过一次 epoch 记录一次数据（准确率）
-        train_acc = network.accuracy(x_train, t_train)  # 算全量训练集准确率，看看模型是不是死记硬背的能力，如果很低说明模型太笨了
-        test_acc = network.accuracy(x_test, t_test)  # 算全量测试集准确率，测试集图片没有参与过计算和更新，看有没有泛化能力
+        train_acc = network.accuracy(x_train, t_train)  # 计算模型在训练集上的准确率，看看模型是不是死记硬背的能力，如果很低说明模型太笨了
+        test_acc = network.accuracy(x_test, t_test)  # 计算模型在测试集上的准确率，测试集图片没有参与过计算和更新，看有没有泛化能力
         train_acc_list.append(train_acc)
         test_acc_list.append(test_acc)
 
         print("训练数据，测试数据 | " + str(train_acc) + "," + str(test_acc))
 
 # 画图
-markers = {'train' : 'o', 'test' : 's'}
+markers = {'train': 'o', 'test': 's'}
 x = np.arange(len(train_acc_list))
 
 plt.plot(x, train_acc_list, label='train_acc')
@@ -63,14 +63,9 @@ plt.ylim(0, 1.0)
 plt.legend(loc='lower right')
 plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
+"""
+总结：
+整个训练过程中，输入数据（x_train、t_train、x_test、t_test）保持不变，模型之所以不断进步，
+是因为 network.params（即 W1、b1、W2、b2）不断更新。
+grad 本身并不会累积变化，它只是每一步根据当前参数重新计算出来的梯度。
+"""
